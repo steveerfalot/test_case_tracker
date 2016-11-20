aea31f1f7c6504113b41fecddc0202ed44a86d16
Mike Parks <mparks@bullhorn.com>
2016-11-19 21:48:08 -0600

    Add cors config and db connection info, remap to /api

--- test_tracker_rest/src/main/java/com/testtracker/controller/StepController.java
+++ test_tracker_rest/src/main/java/com/testtracker/controller/StepController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/step")
+@RequestMapping("/api/step")
 public class StepController {
 
     private StepService stepService;
